035e1196451abb3fae3add6fe167705f62665775
Carlos Pereira <carlos.programacao@gmail.com>
2022-10-26 21:17:56 -0300

    First Commit

---
+++ Verbs/src/main/java/br/com/caps/controller/PersonController.java
@@ -0,0 +1,56 @@
+package br.com.caps.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.caps.model.Person;
+import br.com.caps.service.PersonService;
+
+@RestController
+@RequestMapping(value = "/person")
+public class PersonController {
+
+	@Autowired
+	private PersonService service;
+	// private PersonService service = new PersonService();
+
+	@RequestMapping(method = RequestMethod.GET, 
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Person> findAll() {
+		return service.findAll();
+	}
+	
+	@RequestMapping(value = "{id}", 
+			method = RequestMethod.GET, 
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person findById(@PathVariable(value = "id") String id) {
+		return service.findById(id);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST,
+			consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person create(@RequestBody Person person) {
+		return service.create(person);
+	}
+	
+	@RequestMapping(method = RequestMethod.PUT,
+			consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person update(@RequestBody Person person) {
+		return service.update(person);
+	}
+	
+	@RequestMapping(value = "{id}")
+	public void delete(@PathVariable(value = "id") String id) {
+		service.delete(id);
+	}
+
+}
